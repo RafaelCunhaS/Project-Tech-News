b55c143e620713126c3535234a9c7f5cdcdce4c4
Rafael Santos <rafaelcs_94@hotmail.com>
2022-09-29 17:21:00 -0300

    feat: :sparkles: adds top 5 sort functions

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,22 @@
-# Requisito 10
+from operator import itemgetter
+from tech_news.database import find_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news_sorted = sorted(
+        news, key=itemgetter("comments_count", "title"), reverse=True
+    )
+
+    return [(new["title"], new["url"]) for new in news_sorted[:5]]
 
 
-# Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    categories = [new["category"] for new in news]
+    categories_sorted = sorted(
+        set(categories),
+        key=lambda x: (-categories.count(x), x)
+    )
+
+    return categories_sorted[:5]
