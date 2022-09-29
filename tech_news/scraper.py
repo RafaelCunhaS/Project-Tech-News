abc99d468164bbc3a9635782693e76a65a262e3a
Rafael Santos <rafaelcs_94@hotmail.com>
2022-09-29 14:46:25 -0300

    feat: :sparkles: adds fetch funtion with scrape functions

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,35 @@
-# Requisito 1
+from time import sleep
+from parsel import Selector
+import requests
+# from tech_news.database import create_news
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        headers = {"user-agent": "Fake user-agent"}
+        response = requests.get(url, timeout=3, headers=headers)
+        response.raise_for_status()
+        sleep(1)
+        return response.text
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
 
 
-# Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    news = selector.css("h2.entry-title a::attr(href)").getall()
+    return news
 
 
-# Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page = selector.css("a.next::attr(href)").get()
+    return next_page
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    pass
