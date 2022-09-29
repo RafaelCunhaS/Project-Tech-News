e94526d170354e7b8bfea936359694516ed7d092
Rafael Santos <rafaelcs_94@hotmail.com>
2022-09-29 14:51:36 -0300

    feat: :sparkles: adds search functions

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,39 @@
-# Requisito 6
+from tech_news.database import search_news
+from datetime import date as d
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": 'i'}})
+    return [
+        (new["title"], new["url"])
+        for new in news
+    ]
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        news = search_news(
+            {"timestamp": d.fromisoformat(date).strftime("%d/%m/%Y")}
+        )
+        return [
+            (new["title"], new["url"])
+            for new in news
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
-# Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    news = search_news({"tags": {"$regex": tag, "$options": 'i'}})
+    return [
+        (new["title"], new["url"])
+        for new in news
+    ]
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, "$options": 'i'}})
+    return [
+        (new["title"], new["url"])
+        for new in news
+    ]
